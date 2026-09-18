fc2887340bae0f1c85f0084cc64f425d6507963e
pleniterballet-pixel <pleniterballet@gmail.com>
2026-09-18 12:04:11 -0300

    Delete codigo/sistema/backend directory

--- codigo/sistema/backend/main.py
+++
@@ -1,9 +0,0 @@
-import database
-import bcrypt
-import auth
-import web
-
-#print(auth.login(input("name: "), input("password: ")))
-web.start_server()
-
-#print(auth.login(auth.get_user_uuid(input("email: ")), input("password: ")))
